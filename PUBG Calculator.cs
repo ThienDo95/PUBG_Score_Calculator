dade8c053357fe8fb9798e8fe7a567f41f02bf48
Thien Hoang Tran Do <thiendo411@gmail.com>
2018-07-24 03:18:38 -0400

    Update PUBG Calculator.cs

--- PUBG Calculator.cs
+++ PUBG Calculator.cs
@@ -27,35 +27,5 @@ namespace PUBG_Score_Calculator
             textBox3.Text = p.calculateWholeGame(placement, kill).ToString();
 
         }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox2_TextChanged_1(object sender, EventArgs e)
-        {
-
-        }
     }
 }
